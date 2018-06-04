0b399ed644071dda89d914ed295072c176dfa935
Artem Borisov <dedsa2002@gmail.com>
2018-06-05 00:59:58 +0300

    berkeley: Add proper AAPT configuration

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/huawei/berkeley/berkeley-vendor.mk)
 
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
