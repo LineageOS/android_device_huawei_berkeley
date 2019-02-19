11267a10f748ecb7a83561c1e952db9a87f18ab0
Paul Keith <javelinanddart@gmail.com>
2019-02-19 18:26:25 +0100

    berkeley: Set initial copyright year
    Change-Id: I24c549f6df20316d2597f3af56466d71a70fc180

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,5 +21,7 @@ set -e
 export DEVICE=berkeley
 export DEVICE_COMMON=kirin970-common
 export VENDOR=huawei
+
+export INITIAL_COPYRIGHT_YEAR=2018
 
 ./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
